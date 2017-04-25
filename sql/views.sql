c7950986ae5979be5b72be497b4307bbb86dc680
Kevin R. Farmer <Kevin.R.Farmer.18@dartmouth.edu>
2017-04-25 12:57:28 -0400

    Generated data and created the (empty) views file

---
+++ sql/views.sql
@@ -0,0 +1,32 @@
+/*
+ * For CS61 Lab 2c
+ * Creates views necessary for our application
+ */
+
+USE `kfarmer_db`;
+
+drop view if exists LeadAuthorManuscripts;
+create view LeadAuthorManuscripts as ;
+
+
+drop view if exists AnyAuthorManuscripts;
+create view AnyAuthorManuscripts as ;
+
+
+drop view if exists PublishedIssues;
+create view PublishedIssues as ;
+
+
+drop view if exists ReviewQueue;
+create view ReviewQueue as ;
+
+
+drop view if exists WhatsLeft;
+create view WhatsLeft as ;
+
+
+drop view if exists ReviewStatus;
+create view ReviewStatus as ;
+
+
+
