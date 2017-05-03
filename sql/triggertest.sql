267a25ffda49569da03cddcd60c1059063360672
Kevin R. Farmer <Kevin.R.Farmer.18@dartmouth.edu>
2017-05-02 20:21:04 -0400

    Added disable safe mode

--- sql/triggertest.sql
+++ sql/triggertest.sql
@@ -3,9 +3,13 @@
  * Trigger tests
  */
 
+SET SQL_SAFE_UPDATES = 0;
+
 -- 100 is an unused RI code
 INSERT INTO `manuscript` (`RICodes_code`) VALUES (100)
 -- Delete reviewer 21 who is the only reviewer for his manuscript
 DELETE FROM `reviewer` WHERE reviewer.user_id = 21;
 -- Delete reviewer 22 who is not the only reviewer for his manuscript
 DELETE FROM `reviewer` WHERE reviewer.user_id = 22;
+
+SET SQL_SAFE_UPDATES = 1;
