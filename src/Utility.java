43eb5885a479e2e9a5cfcb94cabab8163fae0944
Yusuf Olokoba <olokobayusuf@gmail.com>
2017-05-13 12:15:47 -0400

    Minor housekeeping

--- src/Utility.java
+++ src/Utility.java
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-=======
-import java.util.Scanner;
-import java.util.Formatter;
-import javax.crypto.*;
-import java.security.*;
-import javax.crypto.spec.SecretKeySpec;
-
-
->>>>>>> 709bc45243e93532372f1e88ae259252e15df12a
 /*
 *   ManuScripts
 *   CS 61 - 17S
@@ -16,8 +6,11 @@ import javax.crypto.spec.SecretKeySpec;
 import java.util.Scanner;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import javax.crypto.spec.SecretKeySpec;
 import java.sql.*;
 import java.util.regex.*;
+import javax.crypto.*;
+import java.security.*;
 
 public class Utility {
 
@@ -91,98 +84,63 @@ public class Utility {
     }
     //endregion
 
+    //region --Encryption--
 
     /**
      * Encryption of a given text using the provided secretKey
-     * 
      * @param text
      * @param secretKey
      * @return the encoded string
      */
-    public static String encrypt(String text, String secretKey) {
+    public static String encrypt (String text, String secretKey) {
         if (secretKey.length() != 16) {
             logError("Secret key must be 16 characters");
             return null;
         }
         try {
             Key sk = new SecretKeySpec(secretKey.getBytes(), "AES");
-
             Cipher aesCipher = Cipher.getInstance("AES");
             aesCipher.init(Cipher.ENCRYPT_MODE, sk);
-
             byte[] results = aesCipher.doFinal(text.getBytes());
             return toHex(results);
-            
-       } catch (NoSuchAlgorithmException e) {
-            logError("NoSuchAlgorithmException when attempting to encrypt");
-            return null;
-        } catch (InvalidKeyException e) {
-            logError("InvalidKeyException when attempting to encrypt");
-            return null;
-        } catch (IllegalBlockSizeException e) {
-            logError("IllegalBlockSizeException when attempting to encrypt");
-            return null;
-        } catch (NoSuchPaddingException e) {
-            logError("NoSuchPaddingException when attempting to encrypt");
-            return null;
-        } catch (BadPaddingException e) {
-            logError("BadPaddingException when attempting to encrypt");
+       } catch (NoSuchAlgorithmException | InvalidKeyException | IllegalBlockSizeException | NoSuchPaddingException | BadPaddingException ex) {
+            logError("Failed to encrypt string: "+ex);
             return null;
         }
     }
 
-    public static String decrypt(String text, String secretKey) {
+    public static String decrypt (String text, String secretKey) {
         if (secretKey.length() != 16) {
             logError("Secret key must be 16 characters");
             return null;
         }
         try {
             Key sk = new SecretKeySpec(secretKey.getBytes(), "AES");
-
             Cipher aesCipher = Cipher.getInstance("AES");
             aesCipher.init(Cipher.DECRYPT_MODE, sk);
-
             byte[] results = aesCipher.doFinal(toByte(text));
             return new String(results);
-            
-       } catch (NoSuchAlgorithmException e) {
-            logError("NoSuchAlgorithmException when attempting to decrypt");
-            return null;
-        } catch (InvalidKeyException e) {
-            logError("InvalidKeyException when attempting to decrypt");
-            return null;
-        } catch (IllegalBlockSizeException e) {
-            logError("IllegalBlockSizeException when attempting to decrypt");
-            return null;
-        } catch (NoSuchPaddingException e) {
-            logError("NoSuchPaddingException when attempting to decrypt");
-            return null;
-        } catch (BadPaddingException e) {
-            logError("BadPaddingException when attempting to decrypt");
+       } catch (NoSuchAlgorithmException | InvalidKeyException | IllegalBlockSizeException | NoSuchPaddingException | BadPaddingException ex) {
+            logError("Failed to decrypt string: "+ex);
             return null;
         }
     }
-
 
     /* 
      * toByte and toHex used from:
      * http://stackoverflow.com/questions/23361224/how-to-generate-a-128-bit-key-to-use-in-aes-algorithm 
      * in order to fix padding issues in encryption
-     *
      */
-    private static byte[] toByte(String data) throws NullPointerException{
-        int len = data.length()/2;
-        byte[] result = new byte[len];
-        for (int i = 0; i < len; i++)
-            result[i] = Integer.valueOf(data.substring(2*i, 2*i+2), 16).byteValue();
+    private static byte[] toByte(String data) throws NullPointerException {
+        byte[] result = new byte[data.length() / 2];
+        for (int i = 0; i < result.length; i++) result[i] = Integer.valueOf(data.substring(2 * i, 2 * i + 2), 16).byteValue();
         return result;
     }
 
-    private static String toHex(byte[] doFinal) {
-        StringBuffer result = new StringBuffer(2*doFinal.length);
-        for (int i = 0; i < doFinal.length; i++) {
-            result.append(HEX.charAt((doFinal[i]>>4)&0x0f)).append(HEX.charAt(doFinal[i]&0x0f));
-        }
+    private static String toHex (byte[] doFinal) {
+        StringBuffer result = new StringBuffer(2 * doFinal.length);
+        for (int i = 0; i < doFinal.length; i++) result.append(HEX.charAt((doFinal[i]>>4)&0x0f)).append(HEX.charAt(doFinal[i]&0x0f));
         return result.toString();
     }
+    //endregion
 }
